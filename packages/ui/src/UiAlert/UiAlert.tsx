efb508085e89bc96cee1c171392556c394f8facf
jenerdev <jenerdev@gmail.com>
2025-08-14 12:58:13 +0800

    deploy

--- packages/ui/src/UiAlert/UiAlert.tsx
+++ packages/ui/src/UiAlert/UiAlert.tsx
@@ -1,20 +1,12 @@
 import { useBEM } from '@tectus/hooks';
 import './UiAlert.scss';
-import { Icon, Icons } from '../Icon';
+
 import { Alert, AlertProps } from '@mui/material';
 
 export interface UiAlertProps {
   severity: AlertProps['severity'];
   children?: AlertProps['children'];
   variant?: AlertProps['variant'];
-
-  // icon?: (typeof Icons)[number];
-  // message: string;
-  // type?: 'success' | 'danger' | 'info' | 'warning';
-  // variant?: 'filled' | 'outlined';
-  // onClose?: () => void;
-  // title?: string;
-  // showCloseButton?: boolean;
 }
 
 export function UiAlert(props: UiAlertProps) {
